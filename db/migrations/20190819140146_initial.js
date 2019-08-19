bf455b8c01b78ea98debfd258619ca19ca8ba635
kobesparrow <markcollins.md@gmail.com>
2019-08-19 14:16:42 -0600

    Schema for board_games table

--- db/migrations/20190819140146_initial.js
+++ db/migrations/20190819140146_initial.js
@@ -2,12 +2,20 @@
 exports.up = function(knex) {
   return Promise.all([
     knex.schema.createTable('board_games', function(table) {
-      table.increments('id').primary,
-      
+      table.increments('id').primary;
+      table.string('name');
+      table.integer('minimum players');
+      table.integer('maximum players');
+      table.string('weight');
+      table.integer('time to play');
+      table.string('type');
+      table.string('image');
+
+      table.timestamps(true, true)
     })
-  ])
+  ]);
 };
 
 exports.down = function(knex) {
-  
+  knex.schema.dropTable('board_games');
 };
